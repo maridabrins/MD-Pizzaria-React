7ef401fbb1dc03ddde521e70626bbd46d82ba706
maridabrins <maridabrins@hotmail.com>
2025-06-23 21:48:54 -0300

    feat: adicionando as rotas de navegação

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import './App.css'
-import Header from './components/header'
-import HeroSection from './components/pageHome/hero-section'
-import HighlightSection from './components/pageHome/highlights-section'
-import Footer from './components/footer'
+import Home from './pages/Home'
+import Menu from './pages/Menu'
+import Contact from './pages/Contact'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <>
-      <Header/>
-      <HeroSection/>
-      <HighlightSection/>
-      <Footer/>
-    </>
+    <BrowserRouter> {/* Onde será agrupado tudo */}
+      <Routes> {/*  Onde será armazenada todas as rotas */}
+        <Route path='/'element={ <Home/> }/> {/* Rotas individuais */}
+        <Route path='/menu'element={ <Menu/> }/> {/* Rotas individuais */}
+        <Route path='/contact'element={ <Contact/> }/> {/* Rotas individuais */}
+      </Routes>
+    </BrowserRouter>
+    // <>
+    // <Home/> 
+    // <Menu/> 
+    // <Contact/>
+    // </>
   )
 }
 
