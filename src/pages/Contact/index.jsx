17d8fb91ad2b90518a8607db9a1e13c628abb01b
maridabrins <maridabrins@hotmail.com>
2025-06-23 21:07:49 -0300

    feat: adicionando page Contact

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,11 +1,12 @@
 import Footer from "../../components/footer"
 import Header from "../../components/header"
+import ContactSection from "../../components/pageContact/contact-section"
 
 const Contact = () => {
     return(
         <>
         <Header/>
-        
+        <ContactSection/>
         <Footer/>
         </>
     )
