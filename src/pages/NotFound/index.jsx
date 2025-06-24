a191a8669ed7e5bffcdaf28d74d2ba12183d8b0a
maridabrins <maridabrins@hotmail.com>
2025-06-24 20:53:19 -0300

    feat: criando page NotFound

---
+++ src/pages/NotFound/index.jsx
@@ -0,0 +1,11 @@
+import SectionNotFound from "../../components/pageNotFound/section-notFound"
+
+
+const NotFound = () => {
+    return(
+        <>
+        <SectionNotFound/>
+        </>
+    )
+}
+export default NotFound
